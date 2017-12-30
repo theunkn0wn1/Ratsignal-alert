043cf768742f1c2f2c94d0bd091f395d1c46fdf3
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2017-12-30 13:33:38 -0800

    Added skeleton for Hooks. began adding RSIG parsing

--- alert.py
+++ alert.py
@@ -16,7 +16,7 @@ except ImportError:
     hex = False
 
 # utilities imports
-from utils.playground.shared_resources import CommandBase, eat_all, Parser, Case
+from utils.playground.shared_resources import CommandBase, eat_all, Parser, Case, hc
 
 logging.basicConfig(format="%(levelname)s :%(message)s")
 logger = logging.getLogger('alerts')
@@ -44,6 +44,57 @@ class HelpCommand(CommandBase):
         :return:
         """
         print("{name} version {version}".format(name=__module_name__, version=__module_version__))
+
+class Hooks():
+    """
+    Hexchat event hooks Hooks and their methods
+    """
+    # ('hookable', method)
+    hooks = [('PRIVMSG')]
+    @classmethod
+    def on_load(cls)->None:
+        """
+        Hex event fired when module is loaded
+        :return: None
+        """
+        pass
+
+    @classmethod
+    def on_unload(cls)->None:
+        """
+        Event fired during module unload
+        :return:
+        """
+        pass
+
+    @classmethod
+    def on_message(cls, word:list, word_eol:list, userdata:None)->int:
+        """
+        Event fired when a message is received
+        :param word: list of space-deliminated words in event
+        :param word_eol: list of space-delimiated words-to-eol
+        :param userdata: Custom data, nothing of interest
+        :return: 3 # equal to HC.EAT_ALL
+        """
+        logger.debug("got data {word}".format(word=word))
+        data:Case = Parser.parse(data=word)
+        if isinstance(data, Case):
+            logger.info("Received RATSIGNAL event, got case data!")
+            logger.debug("RSIG data: client={client}, plaform={plat}, cr={cr}".format(client=data.client,
+                                                                                      plat=data.platform,
+                                                                                      cr = data.cr))
+        else:
+            logger.info("Unknown event data")
+            logger.debug("event was of type {} obj = {}".format(type(data), data))
+    def __init__(self):
+        """
+        Init the event hooks
+        """
+        if hc:
+            pass
+
+
+
 def init()->None:
     """
     Module init
