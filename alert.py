61910b1e5c6a63528f5d4e7028582cfb6cfdf38d
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2017-12-30 22:32:06 -0800

    fixed make_default_config() added CLI option `-make` to generate the default configs

--- alert.py
+++ alert.py
@@ -47,33 +47,33 @@ def init()->None:
     log.debug("Init: creating config parser....")
     config_parser = ConfigParser()
     log.info("Init: reading config file...")
-    config = config_parser.read('config.ini')
-    if config == [] :
-        log.warning("Config file not found. generating defaults...")
-        with open('config.ini', 'w'):
-            log.warning("config file was not found or blank. writing new...")
-            # config was null, first run ?
-            section = 'files'
-            config_parser.add_section(section)
-            config_parser.set(section=section, option='channel_fuelrats', value='/path/to/#fuelrats')
-            config_parser.write('config.ini')
-    try:
-        log.debug("fetching log data....")
-        logfile = config_parser.get('files', 'channel_fuelrats')
-        log.info("got logfile data, path is {}".format(logfile))
-    except Exception as ex:
-        log.critical("Unable to find log file configuration.")
-        with open('config.ini', 'w') as file:
-            log.warning("config file was not found or blank. writing new...")
-            # config was null, first run ?
-            section = 'files'
-            config_parser.add_section(section)
-            config_parser.set(section=section, option='channel_fuelrats', value='/path/to/#fuelrats')
-            config_parser.write(file)
-    log.warning(config)
+    # config = config_parser.read('config.ini')
+    # if config == [] :
+    #     log.warning("Config file not found. generating defaults...")
+    #     with open('config.ini', 'w'):
+    #         log.warning("config file was not found or blank. writing new...")
+    #         # config was null, first run ?
+    #         section = 'files'
+    #         config_parser.add_section(section)
+    #         config_parser.set(section=section, option='channel_fuelrats', value='/path/to/#fuelrats')
+    #         config_parser.write('config.ini')
+    # try:
+    #     log.debug("fetching log data....")
+    #     logfile = config_parser.get('files', 'channel_fuelrats')
+    #     log.info("got logfile data, path is {}".format(logfile))
+    # except Exception as ex:
+    #     log.critical("Unable to find log file configuration.")
+    #     with open('config.ini', 'w') as file:
+    #         log.warning("config file was not found or blank. writing new...")
+    #         # config was null, first run ?
+    #         section = 'files'
+    #         config_parser.add_section(section)
+    #         config_parser.set(section=section, option='channel_fuelrats', value='/path/to/#fuelrats')
+    #         config_parser.write(file)
+    # log.warning(config)
 
 
-def make_default_config_file(parser:ConfigParser, filename ='config.ini', filemode = 'w')->None:
+def make_default_config_file(parser:ConfigParser=None, filename ='config.ini', filemode = 'w')->None:
     """
     Generates a default config filename
     :param parser: config parser instance
@@ -81,7 +81,8 @@ def make_default_config_file(parser:ConfigParser, filename ='config.ini', filemo
     :param filemode: mode to open file, must be capable of writing
     :return: None
     """
-
+    if not parser:
+        parser = ConfigParser()
     # make filename configs
     section = 'files'
     log.info("making {} configs....".format(section))
@@ -92,12 +93,12 @@ def make_default_config_file(parser:ConfigParser, filename ='config.ini', filemo
     # make options configs
     section = 'options'
     log.info("making {} configs....".format(section))
-    parser.add_section("filter")
-    parser.set(section, 'onPlatform', False)
-    parser.set(section, "platform", "pc")
-    parser.set(section, "onLanguage", False)
+    parser.add_section(section)
+    parser.set(section, 'onPlatform', "0")
+    parser.set(section, "platform", str(['pc','xb','ps']))
+    parser.set(section, "onLanguage", "0")
     parser.set(section, "language", "en")
-    parser.set(section, "onCodeRed", False)
+    parser.set(section, "onCodeRed", "0")
     log.info("done initializing new configs, writing to disk.")
     # write to config filename
     try:
@@ -113,9 +114,13 @@ if __name__ == "__main__":
     args = sys.argv
     if len(args)-1 >=1:
         # we have CLI arguments
+        log.debug("got CLI arguments, arguments are {}".format(args[1:]))
         log.debug(args)
+        if "-make" in args:
+            make_default_config_file()
         pass
-    log.debug("No CLI arguments")
+    else:
+        log.debug("No CLI arguments")
 
     init()
 
